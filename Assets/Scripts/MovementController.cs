7d6397b189373be75456fce3f55237b36171173c
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2024-12-20 22:44:54 +0530

    muzzle flash

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,12 +1,15 @@
 using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class MovementController: MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Camera cam;
     
     [Header("Movement Settings")]
-    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] public float walkSpeed = 6f;
+    [SerializeField] public float sprintSpeed = 9f;
+    private float moveSpeed ;
     [SerializeField] public float groundDrag = 6f;
     
     [Header("Jump Settings")]
@@ -14,6 +17,11 @@ public class MovementController: MonoBehaviour
     [SerializeField] public float jumpCooldown = 0.25f;
     [SerializeField] public float airMultiplier = 0.5f;
     [SerializeField] public bool readyToJump = true;
+    private bool exitingSlope;
+    
+    [Header("Slope Settings")]
+    [SerializeField] public float maxSlopAngle = 30f;
+    private RaycastHit slopeHit;
     
     [Header("Ground Check")]
     [SerializeField] private bool grounded;
@@ -24,7 +32,19 @@ public class MovementController: MonoBehaviour
     Rigidbody rb;
     
     private InputHandler input;
+
+    public bool freeze;
     
+    private bool activeGrapple;
+    
+    public MovementState movementState;
+    public enum MovementState
+    {
+        Freeze,
+        Walking,
+        Sprinting,
+        Air,
+    }
 
     private void Awake()
     {
@@ -41,7 +61,7 @@ public class MovementController: MonoBehaviour
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
 
-        if (grounded)
+        if (grounded && !activeGrapple)
         {
             rb.linearDamping = groundDrag;
         }
@@ -49,6 +69,7 @@ public class MovementController: MonoBehaviour
         {
             rb.linearDamping = 0;
         }
+        StateHandler();
     }
     private void FixedUpdate()
     {
@@ -66,36 +87,153 @@ public class MovementController: MonoBehaviour
     
     private void MovePlayer()
     {
+        if (activeGrapple) return;
+        
         moveDirection = orientation.forward * input.MoveInput.y + orientation.right * input.MoveInput.x;
+        
+        if (OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * (moveSpeed * 10f), ForceMode.Force);
+            
+            if (rb.linearVelocity.y < 0)
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            } 
+        }
+        
         if (grounded)
         {
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+            rb.AddForce(moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
         }
         else
         {
-            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier * 10f, ForceMode.Force);
+            rb.AddForce(moveDirection.normalized * (moveSpeed * airMultiplier * 10f), ForceMode.Force);
+        }
+        
+        rb.useGravity = !OnSlope();
+    }
+    
+    private void SpeedControl()
+    {
+        // Limit speed on slope
+        if (OnSlope() && !exitingSlope)
+        {
+            if (rb.linearVelocity.magnitude > moveSpeed)
+            {
+                rb.linearVelocity= rb.linearVelocity.normalized * moveSpeed;
+            }
+        }
+        // Limit speed in general
+        else
+        {
+            Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+
+            if (flatVelocity.magnitude > moveSpeed)
+            {
+                rb.linearVelocity = new Vector3(flatVelocity.normalized.x * moveSpeed, rb.linearVelocity.y, flatVelocity.normalized.z * moveSpeed);
+            }
         }
     }
     
-    [Obsolete("Obsolete")]
-    private void SpeedControl()
+    private void StateHandler()
     {
-        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-
-        if (flatVelocity.magnitude > moveSpeed)
+        //Mode - Freeze
+        if (freeze)
         {
-            rb.linearVelocity = new Vector3(flatVelocity.normalized.x * moveSpeed, rb.linearVelocity.y, flatVelocity.normalized.z * moveSpeed);
+            movementState = MovementState.Freeze;
+            moveSpeed = 0;
+            rb.linearVelocity = Vector3.zero;
+        }
+        //Mode - Sprinting
+        else if (input.SprintTriggered && grounded)
+        {
+            movementState = MovementState.Sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        //Mode - Walking
+        else if (grounded)
+        {
+            movementState = MovementState.Walking;
+            moveSpeed = walkSpeed;
+        }
+        //Mode - Air
+        else
+        {
+            movementState = MovementState.Air;
         }
     }
     
     private void Jump()
     {
-        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        exitingSlope = true;
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
     
     private void ResetJump()
     {
         readyToJump = true;
+        exitingSlope = false;
+    }
+    
+    private bool enableMovementOnNextTouch;
+    public void JumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+
+        velocityToSet = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+        Invoke(nameof(SetVelocity), 0.1f);
+
+        Invoke(nameof(ResetRestrictions), 3f);
+    }
+
+    private Vector3 velocityToSet;
+    private void SetVelocity()
+    {
+        enableMovementOnNextTouch = true;
+        rb.linearVelocity = velocityToSet;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (enableMovementOnNextTouch)
+        {
+            enableMovementOnNextTouch = false;
+            ResetRestrictions();
+        }
+    }
+
+    private void ResetRestrictions()
+    {
+        activeGrapple = false;
+        GetComponent<Grappling>().StopGrapple();
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, 1.1f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopAngle && angle != 0;
+        }
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+    
+    private Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity) 
+                                               + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+        Debug.Log(velocityXZ);
+        return velocityXZ*10f + velocityY;
     }
 }
--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerCam : MonoBehaviour
 {
