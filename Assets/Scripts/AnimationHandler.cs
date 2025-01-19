085233700d3513af500f06645f74dce422e0685d
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2025-01-19 21:41:34 +0530

    Turn Animations
    Turning animation Logics
    Fixed falling animation
    Fixed arms

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -6,7 +6,10 @@ public class AnimationHandler : MonoBehaviour
     [Header("References")]
     [SerializeField] private InputHandler input;
     [SerializeField] private PlayerMovement pm;
-    [SerializeField] private Animator lowerBodyAnim;
+    [SerializeField] private GameObject lowerBody;
+    [SerializeField] private GameObject shadowBody;
+    [SerializeField] private Transform orientation;
+    
     
     [Header("Settings")]
     [SerializeField] private float xVelocity;
@@ -15,6 +18,7 @@ public class AnimationHandler : MonoBehaviour
     public float increaseSpeed = 2f; // Speed of the increase
     public float decreaseSpeed = 1f; // Target value for the x-axis
     private float targetValue;
+    [SerializeField] private float angleDifference;
     
     private bool idle = true;
     private bool jumped;
@@ -26,6 +30,24 @@ public class AnimationHandler : MonoBehaviour
     private readonly int idleHash = Animator.StringToHash("idel");
     private readonly int jumpedHash = Animator.StringToHash("jumped");
     private readonly int inAirHash = Animator.StringToHash("inAir");
+    private readonly int turnHash = Animator.StringToHash("turn");
+    private readonly int turnRight90Hash = Animator.StringToHash("Right Turn 90");
+    private readonly int turnLeft90Hash = Animator.StringToHash("Left Turn 90");
+    private Animator lowerBodyAnimator;
+    private Animator shadowBodyAnimator;
+    private lower lowerBodyScript;
+    private Transform lowerBodyTransform;
+    private Transform shadowBodyTransform;
+
+    private void Start()
+    {
+        lowerBodyAnimator = lowerBody.GetComponent<Animator>();
+        lowerBodyScript = lowerBody.GetComponent<lower>();
+        lowerBodyTransform = lowerBody.transform;
+        
+        shadowBodyAnimator = shadowBody.GetComponent<Animator>();
+        shadowBodyTransform = shadowBody.transform;
+    }
 
     void Update()
     {
@@ -67,20 +89,72 @@ public class AnimationHandler : MonoBehaviour
             xVelocity = Mathf.Lerp(xVelocity, targetValue*inputVector.x, Time.deltaTime * decreaseSpeed);
         }
 
+        if (!lowerBodyScript.rootMotion)
+        {
+            lowerBodyAnimator.applyRootMotion = false;
+            shadowBodyAnimator.applyRootMotion = false;
+            lowerBodyScript.rootMotion = true;
+        }
+        
+        // Calculate the difference in the Y-axis (yaw)
+        float yawDifference = Mathf.DeltaAngle(lowerBodyTransform.eulerAngles.y, orientation.eulerAngles.y);
+        
+        // Turn the player to the direction of the camera
+        if (pm.rb.linearVelocity.magnitude > 0.1f)
+        {
+            lowerBodyAnimator.SetBool(turnHash, true);
+            lowerBodyAnimator.applyRootMotion = false;
+            lowerBodyTransform.rotation = Quaternion.Euler(0, orientation.eulerAngles.y, 0);
+            shadowBodyTransform.rotation = Quaternion.Euler(0, orientation.eulerAngles.y, 0);
+        }
+        // Check if the difference is approximately 80 degrees
+        else if (Mathf.Abs(yawDifference) >= 80)
+        {
+            lowerBodyAnimator.applyRootMotion = true;
+            shadowBodyAnimator.applyRootMotion = true;
+            
+            if (yawDifference > 0)
+            {
+                // Turned 80 degrees to the right
+                lowerBodyAnimator.Play(turnRight90Hash);
+                shadowBodyAnimator.Play(turnRight90Hash);
+            }
+            else
+            {
+                // Turned 80 degrees to the left
+                lowerBodyAnimator.Play(turnLeft90Hash);
+                shadowBodyAnimator.Play(turnLeft90Hash);
+            }
+            
+            if (Mathf.Abs(yawDifference) >= 120)
+            {
+                lowerBodyTransform.rotation= Quaternion.Euler(0, orientation.eulerAngles.y, 0);
+                shadowBodyTransform.rotation= Quaternion.Euler(0, orientation.eulerAngles.y, 0);
+            }
+        }
+
         // Set the xVelocity and yVelocity parameters in the Animator
-        lowerBodyAnim.SetFloat(xVelocityHash, xVelocity);
-        lowerBodyAnim.SetFloat(yVelocityHash, yVelocity);
+        lowerBodyAnimator.SetFloat(xVelocityHash, xVelocity);
+        lowerBodyAnimator.SetFloat(yVelocityHash, yVelocity);
+        
+        // Set the xVelocity and yVelocity parameters in the Animator
+        shadowBodyAnimator.SetFloat(xVelocityHash, xVelocity);
+        shadowBodyAnimator.SetFloat(yVelocityHash, yVelocity);
         
         // Set the idle parameter in the Animator
         idle = pm.rb.linearVelocity.magnitude < 0.1f;
-        lowerBodyAnim.SetBool(idleHash,idle);
+        lowerBodyAnimator.SetBool(idleHash,idle);
+        shadowBodyAnimator.SetBool(idleHash,idle);
+        
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            lowerBodyAnim.Play("Jumping Up");
+            lowerBodyAnimator.Play("Jumping Up");
+            shadowBodyAnimator.Play("Jumping Up");
         }
 
         inAir = !(pm.isGrounded);
-        lowerBodyAnim.SetBool(inAirHash,inAir);
+        lowerBodyAnimator.SetBool(inAirHash,inAir);
+        shadowBodyAnimator.SetBool(inAirHash,inAir);
     }
 }
---
+++ Assets/lower.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class lower : MonoBehaviour
+{
+    public bool rootMotion = true;
+
+    public void ToggleBool()
+    {
+        rootMotion = false;
+    }
+}
