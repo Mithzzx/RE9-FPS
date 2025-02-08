2ad5b3da58f05d5f6c424b34588335809ed923df
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2025-02-08 07:57:50 +0530

    changes

--- Assets/NextActionCondition.cs
+++ Assets/NextActionCondition.cs
@@ -1,40 +1,21 @@
 using System;
 using Unity.Behavior;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [Serializable, Unity.Properties.GeneratePropertyBag]
-[Condition(name: "NextAction", story: "Can Eat dead Zombie in [Range]", category: "Conditions", id: "1097021c6822a70662726386287d6e4d")]
+[Condition(name: "NextAction", story: "Check if AI can eat dead zombie", category: "Conditions")]
 public partial class NextActionCondition : Condition
 {
-    [FormerlySerializedAs("Range")] [SerializeReference] public BlackboardVariable<RangeDetector> range;
-    [SerializeField] public BlackboardVariable<float> percentage = new BlackboardVariable<float>(30f); // Percentage chance to return true
+    [SerializeReference] public BlackboardVariable<RangeDetector> range;
+    [SerializeField, Range(0, 100)] private float percentage = 30f;
+    
+    private static readonly System.Random Random = new System.Random();
 
     public override bool IsTrue()
     {
-        if (!range.Value)
-        {
-            return false;
-        }
-
-        // Check if there is a dead zombie in range
-        bool isDeadZombieInRange = range.Value.IsDeadZombieInHearingRange();
-
-        // Return true based on the specified percentage
-        if (isDeadZombieInRange)
-        {
-            Debug.Log("Can Eat dead Zombie in [Range]");
-            return UnityEngine.Random.Range(0f, 100f) < percentage;
-        }
-        Debug.Log("Can't Eat dead Zombie in [Range]");
-        return false;
-    }
-
-    public override void OnStart()
-    {
-    }
-
-    public override void OnEnd()
-    {
+        if (range?.Value == null) return false;
+        
+        return range.Value.IsDeadZombieInRange() && 
+               Random.NextDouble() * 100 < percentage;
     }
 }
